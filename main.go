63fb6adf12fad486263bb46f48b5a8f2ad8fa50e
HanYaaaaaaa <145742863+HanYaaaaaaa@users.noreply.github.com>
2023-11-26 06:19:10 +0800

    Update main.go

--- main.go
+++ main.go
@@ -258,8 +258,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 2095598013)
-	// sus = append(sus, 1178876832)
+	 sus = append(sus, 2095598013)
+	 sus = append(sus, 1178876832)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
@@ -287,7 +287,7 @@ func init() {
 	}
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:       append([]string{*adana}, "咖波bot"),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
